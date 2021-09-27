acc89487d32a4f7b9799b6222ddc79670769883a
Fabian Offert <fabian.offert@gmail.com>
2021-09-27 12:09:29 -0700

    Added new functions

--- toolbox.py
+++ toolbox.py
@@ -2,6 +2,7 @@ import requests
 import PIL.Image
 from io import BytesIO
 import time
+import os
 
 # Returns an image from an URL as a PIL image
 def img_from_url(url, max_tries=10):
@@ -15,3 +16,10 @@ def img_from_url(url, max_tries=10):
         except:
             tries += 1
         time.sleep(1)
+
+def load_img(path):
+    return PIL.Image.open(path).convert("RGB")
+
+def new_dir(folder):
+    os.makedirs(folder, exist_ok=True)
+    return folder
